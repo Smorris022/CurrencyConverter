370afbda3d24bdb460ae6059eb3e2489f5b5b918
Stephan Morris <>
2018-07-11 16:20:27 -0400

    Final Version

--- CurrencyConverter/ViewController.m
+++ CurrencyConverter/ViewController.m
@@ -7,21 +7,54 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface ViewController ()
+
+@interface ViewController () <CRCurrencyRequestDelegate>
+
+@property (nonatomic) CRCurrencyRequest *req;
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UILabel *currencyA;
+@property (weak, nonatomic) IBOutlet UILabel *currencyB;
+@property (weak, nonatomic) IBOutlet UILabel *currencyC;
+
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)buttonTapped:(id)sender {
+    //button method to convert currency
+    self.convertButton.enabled = NO;
+    
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies{
+    self.convertButton.enabled = YES;
+    
+    double inputValue = [self.inputField.text floatValue];
+    
+    double euroValue = inputValue * currencies.EUR;
+    NSString *temp = [NSString stringWithFormat:@"%.2f", euroValue];
+    
+    double yenValue = inputValue * currencies.JPY;
+    NSString *tempJPY = [NSString stringWithFormat:@"%.2f", yenValue];
+    
+    double gbdValue = inputValue * currencies.GBP;
+    NSString *tempGBD = [NSString stringWithFormat:@"%.2f", gbdValue];
+    
+    
+    self.currencyA.text = temp;
+    self.currencyB.text = tempJPY;
+    self.currencyC.text = tempGBD;
 }
 
 @end
